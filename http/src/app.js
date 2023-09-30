2f1b935231a35ba6d5c51f3cff57d45743bd57f0
ItaloF532 <italo.francisco@startse.com>
2023-09-30 11:55:06 -0300

    refactor: adjust app to use auth router

--- http/src/app.js
+++ http/src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import authRouter from "./infra/routes/auth.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(authRouter)
 
 app.listen(8080, () => {
   console.log('HTTP Running on port 8080.');
