c54ae5830837e91f67121ab0cc84021f28ea568d
ItaloF532 <italo.francisco@startse.com>
2023-09-30 16:33:51 -0300

    feat: add cache strategy on chats

--- http/src/infra/controllers/ChatController.js
+++ http/src/infra/controllers/ChatController.js
@@ -1,14 +1,26 @@
 import MongooseChatProvider from "../../providers/mongoose/chats/providers/MongooseChatProvider.js";
+import RedisCacheProvider from "../../providers/redis/RedisCacheProvider.js";
 
 class ChatController {
+  CHAT_CACHE_KEY = "chats";
   chatProvider = new MongooseChatProvider();
+  cacheProvider = RedisCacheProvider.getInstance();
 
   async createChat(userId, partnerId) {
+    await this.cacheProvider.invalidate(`${this.CHAT_CACHE_KEY}-${userId}`);
     return await this.chatProvider.createChat([userId, partnerId]);
   }
 
   async getUserChats(userId) {
-    return await this.chatProvider.getChatsByUserId(userId);
+    let userChats = await this.cacheProvider.recover(
+      `${this.CHAT_CACHE_KEY}-${userId}`
+    );
+
+    if (userChats) return userChats;
+
+    userChats = await this.chatProvider.getChatsByUserId(userId);
+    this.cacheProvider.save(`${this.CHAT_CACHE_KEY}-${userId}`, userChats, 120);
+    return userChats;
   }
 }
 
