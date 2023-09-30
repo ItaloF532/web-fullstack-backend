1b29fde5070c4df6edb8f9603488ffebd652f185
ItaloF532 <italo.francisco@startse.com>
2023-09-30 14:39:32 -0300

    feat: create chat provider with create and get chats method

---
+++ http/src/providers/chats/providers/MongooseChatProvider.js
@@ -0,0 +1,20 @@
+import chatModel from "../models/ChatModel.js";
+
+class MongooseChatProvider {
+  async createChat(users) {
+    const createdChat = await chatModel.create({
+      users,
+    });
+
+    return createdChat._id.toString();
+  }
+
+  async getChatsByUserId(userId) {
+    const aggregation = `{ audiences: { $in: [ ObjectId('${userId}') ] } }`;
+    const chats = await chatModel.aggregate(aggregation);
+    console.log(chats);
+    return chats;
+  }
+}
+
+export default MongooseChatProvider;
