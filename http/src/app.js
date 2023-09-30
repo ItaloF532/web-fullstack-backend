4cb13338c52c3561afe324ae2854d9fbe4b37d0f
ItaloF532 <italo.francisco@startse.com>
2023-09-30 12:33:24 -0300

    feat: import connection on app to open connection on app run

--- http/src/app.js
+++ http/src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
+import "./providers/connection.js";
 import authRouter from "./infra/routes/auth.routes.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(authRouter)
+app.use(authRouter);
 
 app.listen(8080, () => {
-  console.log('HTTP Running on port 8080.');
+  console.log("HTTP Running on port 8080.");
 });
