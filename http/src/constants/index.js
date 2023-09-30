c61490eb708e7744e6310205da5c4623c1316b24
ItaloF532 <italo.francisco@startse.com>
2023-09-30 12:49:18 -0300

    feat: create data base uri constant

--- http/src/constants/index.js
+++ http/src/constants/index.js
@@ -1,3 +1,5 @@
-const SECRET_KEY = 'aHR0cC1mb3ItYXM2NGEtc3VmZmljaWVuY3ktZXhhbQ==';
+const SECRET_KEY = "aHR0cC1mb3ItYXM2NGEtc3VmZmljaWVuY3ktZXhhbQ==";
+const DATA_BASE_URI =
+  "mongodb://admin:aHR0cC1mb3ItYXM2NGEtc3VmZmljaWVuY3ktZXhhbQ%3D%3D@localhost:27017/";
 
-export { SECRET_KEY }; 
+export { SECRET_KEY, DATA_BASE_URI };
