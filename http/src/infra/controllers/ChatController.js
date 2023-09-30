2bd6e1c2cc9624381c089e98861af4b8035fa96a
ItaloF532 <italo.francisco@startse.com>
2023-09-30 16:09:06 -0300

    feat: create chat controller

---
+++ http/src/infra/controllers/ChatController.js
@@ -0,0 +1,15 @@
+import MongooseChatProvider from "../../providers/chats/providers/MongooseChatProvider.js";
+
+class ChatController {
+  chatProvider = new MongooseChatProvider();
+
+  async createChat(userId, partnerId) {
+    return await this.chatProvider.createChat([userId, partnerId]);
+  }
+
+  async getUserChats(userId) {
+    return await this.chatProvider.getChatsByUserId(userId);
+  }
+}
+
+export default ChatController;
