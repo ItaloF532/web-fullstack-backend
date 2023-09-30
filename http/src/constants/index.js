29b0ac8db40f1959f06bcda154dfb899abfa31a6
ItaloF532 <italo.francisco@startse.com>
2023-09-30 16:18:33 -0300

    feat: create redis constants

--- http/src/constants/index.js
+++ http/src/constants/index.js
@@ -1,5 +1,10 @@
 const SECRET_KEY = "aHR0cC1mb3ItYXM2NGEtc3VmZmljaWVuY3ktZXhhbQ==";
+
 const DATA_BASE_URI =
   "mongodb://admin:aHR0cC1mb3ItYXM2NGEtc3VmZmljaWVuY3ktZXhhbQ%3D%3D@localhost:27017/";
 
-export { SECRET_KEY, DATA_BASE_URI };
+const REDIS_PORT = 6379;
+
+const REDIS_HOST = "localhost";
+
+export { SECRET_KEY, DATA_BASE_URI, REDIS_HOST, REDIS_PORT };
