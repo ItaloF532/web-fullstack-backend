a47d09a7bcc83f7c7643d5a43148ec7b8308b965
ItaloF532 <italo.francisco@startse.com>
2023-09-30 18:39:38 -0300

    feat: make http secure

--- http/src/app.js
+++ http/src/app.js
@@ -1,16 +1,21 @@
+import fs from "fs";
+import https from "https";
 import express from "express";
 import "./providers/mongoose/connection.js";
 import authRouter from "./infra/routes/auth.routes.js";
 import userRouter from "./infra/routes/user.routes.js";
 import chatRouter from "./infra/routes/chat.routes.js";
 
+const key = fs.readFileSync("/Users/italo/key.pem");
+const cert = fs.readFileSync("/Users/italo/cert.pem");
 const app = express();
+const server = https.createServer({ key: key, cert: cert }, app);
 
 app.use(express.json());
 app.use(authRouter);
 app.use(userRouter);
 app.use(chatRouter);
 
-app.listen(8080, () => {
-  console.log("HTTP Running on port 8080.");
+server.listen(8080, () => {
+  console.log("HTTPS Running on port 8080.");
 });
