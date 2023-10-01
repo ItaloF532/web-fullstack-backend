a8c48f12d82a23b7093bcf167e2e7d360b13d3c2
ItaloF532 <italo.francisco@startse.com>
2023-10-01 11:16:21 -0300

    feat: create ws connection

--- web-socket/src/app.js
+++ web-socket/src/app.js
@@ -1,12 +1,23 @@
-import http from "node:http";
+import { WebSocketServer } from "ws";
+// import * as amqp from "amqplib";
+import { authMiddleware } from "./infra/middlewares/auth.middleware.js";
+import ChatMessageController from "./infra/controllers/ChatMessageController.js";
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Server is runing!",
-    })
-  );
+const wss = new WebSocketServer({ port: 8000 });
+const chatMessageController = new ChatMessageController();
+
+wss.on("connection", function connection(ws, request) {
+  ws.on("error", console.error);
+  console.log(request.headers);
+  const userIsAuthenticated = authMiddleware(request);
+
+  console.log(userIsAuthenticated);
+  ws.on("message", async (data, request) => {
+    console.log(data);
+    // if (!userIsAuthenticated) {
+    //   ws.close(1008, "Authentication failed");
+    //   return;
+    // }
+    // chatMessageController.postMessage(data);
+  });
 });
-
-server.listen(8000);
