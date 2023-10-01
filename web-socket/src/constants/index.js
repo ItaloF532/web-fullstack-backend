afea1f68a1cd96086279b60bb356fd6524f10f19
ItaloF532 <italo.francisco@startse.com>
2023-10-01 11:09:40 -0300

    feat: create quee constants

--- web-socket/src/constants/index.js
+++ web-socket/src/constants/index.js
@@ -1,3 +1,7 @@
 const SECRET_KEY = "aHR0cC1mb3ItYXM2NGEtc3VmZmljaWVuY3ktZXhhbQ==";
 
-export { SECRET_KEY };
+const CHAT_MSG_URL = `amqp://admin:${SECRET_KEY}@localhost:5672/`;
+
+const CHAT_MSG_QUEE_NAME = "chat-message";
+
+export { SECRET_KEY, CHAT_MSG_URL, CHAT_MSG_QUEE_NAME };
