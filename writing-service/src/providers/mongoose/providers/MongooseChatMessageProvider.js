be278c1a441184abd31a9de1ee3a2d5125d1bba2
ItaloF532 <italo.francisco@startse.com>
2023-10-01 12:17:20 -0300

    feat: create chat message provider

---
+++ writing-service/src/providers/mongoose/providers/MongooseChatMessageProvider.js
@@ -0,0 +1,24 @@
+import chatMessageModel from "../models/ChatMessageModel";
+
+class MongooseChatMessageProvider {
+  async writeMessage(chatId, userId, message) {
+    const chat = await chatMessageModel.findOne({ chatId });
+    chat.messages.push({
+      userId,
+      message,
+      createdAt: new Date().toISOString(),
+    });
+
+    chatMessageModel.findOneAndUpdate(
+      {
+        chatId,
+      },
+      {
+        messages: chat.messages,
+      },
+      { upsert: true, new: true }
+    );
+  }
+}
+
+export default MongooseChatMessageProvider;
