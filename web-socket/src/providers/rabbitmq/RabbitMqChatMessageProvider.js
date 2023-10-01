043d2d1bfa9eee1c77e7feb9afaf591909ad2901
ItaloF532 <italo.francisco@startse.com>
2023-10-01 11:17:09 -0300

    feat: create quee provider

---
+++ web-socket/src/providers/rabbitmq/RabbitMqChatMessageProvider.js
@@ -0,0 +1,24 @@
+import * as amqp from "amqplib";
+import { CHAT_MSG_QUEE_NAME } from "../../constants/index.js";
+
+class RabbitMqChatMessageProvider {
+  async sendMessage(chatId, userId, message) {
+    const connection = await amqp.connect(CHAT_MSG_URL);
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(CHAT_MSG_QUEE_NAME, { durable: true });
+
+    const chatMessage = {
+      chatId,
+      userId,
+      message,
+    };
+
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(chatMessage)));
+
+    await channel.close();
+    await connection.close();
+  }
+}
+
+export default RabbitMqChatMessageProvider;
