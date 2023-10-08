bd9aa1d9d4df91db6e1148af30c8922d2a604918
ItaloF532 <italo.francisco@startse.com>
2023-10-08 14:32:47 -0300

    feat: ad envs on http

--- http/src/constants/index.js
+++ http/src/constants/index.js
@@ -1,10 +1,9 @@
-const SECRET_KEY = "aHR0cC1mb3ItYXM2NGEtc3VmZmljaWVuY3ktZXhhbQ==";
+const SECRET_KEY = process.env.SECRET_KEY;
 
-const DATA_BASE_URI =
-  "mongodb://admin:aHR0cC1mb3ItYXM2NGEtc3VmZmljaWVuY3ktZXhhbQ%3D%3D@localhost:27017/";
+const DATA_BASE_URI = process.env.DATA_BASE_URI;
 
-const REDIS_PORT = 6379;
+const REDIS_PORT = process.env.REDIS_PORT;
 
-const REDIS_HOST = "localhost";
+const REDIS_HOST = process.env.REDIS_HOST;
 
 export { SECRET_KEY, DATA_BASE_URI, REDIS_HOST, REDIS_PORT };
