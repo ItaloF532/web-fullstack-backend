23924fd360f3b8983ff0adfc5f2a272f0acdfcfc
ItaloF532 <italo.francisco@startse.com>
2023-10-08 14:28:20 -0300

    feat: create env template

--- writing-service/src/constants/index.js
+++ writing-service/src/constants/index.js
@@ -1,10 +1,9 @@
-const DATA_BASE_URI =
-  "mongodb://admin:aHR0cC1mb3ItYXM2NGEtc3VmZmljaWVuY3ktZXhhbQ%3D%3D@localhost:27017/";
+const DATA_BASE_URI = process.env.DATA_BASE_URI;
 
-const SECRET_KEY = "aHR0cC1mb3ItYXM2NGEtc3VmZmljaWVuY3ktZXhhbQ==";
+const SECRET_KEY = process.env.SECRET_KEY;
 
 const CHAT_MSG_URL = `amqp://admin:${SECRET_KEY}@localhost:5672/`;
 
-const CHAT_MSG_QUEE_NAME = "chat-message";
+const CHAT_MSG_QUEE_NAME = process.env.CHAT_MSG_QUEE_NAME;
 
 export { DATA_BASE_URI, SECRET_KEY, CHAT_MSG_QUEE_NAME, CHAT_MSG_URL };
